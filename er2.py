c7af10f883291b4381273c3c41374d8af859497b
rotzinger <rotzinger>
2012-07-27 13:31:39 +0000

    major changes to er

--- er2.py
+++ er2.py
@@ -1,268 +1,67 @@
 
-from numpy import arange
-
+#from numpy import arange,delete,append, zeros
 # Enthought library imports
-try:
-	from enthought.enable.api import Window, Component, ComponentEditor
-	from enthought.traits.api import HasTraits, Instance ,Float, Button,false
-	from enthought.traits.ui.api import Item, Group,VGroup,HGroup, View, HSplit, VSplit, HFlow
-	# Chaco imports
-	from enthought.chaco.api import HPlotContainer, ArrayPlotData, Plot,VPlotContainer
-except:
-        from enable.api import Window, Component, ComponentEditor
-        from traits.api import HasTraits, Instance ,Float, Button,false
-        from traitsui.api import Item, Group,VGroup,HGroup, View, HSplit, VSplit, HFlow
-        # Chaco imports
-        from chaco.api import HPlotContainer, ArrayPlotData, Plot,VPlotContainer
+#from time import sleep
+#from threading import Thread, Lock
 
-from time import sleep
-from threading import Thread, Lock
-
-# import modules
-import lib.er_ratedev as r_dev
-#from lib.er_ftdidev import er_output as o_dev
-import lib.tip_pidcontrol as er_pid
-
-
-# data share
-class data(object):
-    "data object"
-    def __init__(self):
-        # define operational variables
-        self.m_thickness = 0
-        self.m_rate    = 0
-        self.m_time    = 0
-        self.m_error   = 0
-        self.o_new_val = 0
-        self.pid_P = 0
-        self.pid_I = 0
-        self.pid_D = 0
-
-        self.pid = 0
-        self.r_dev = 0
-        
-        self.lock = Lock()
-        
-    def get_thickness(self):
-        return self.m_thickness
-    def set_thickness(self,tn):
-        with self.lock:
-            self.m_thickness = tn
-        
-    def get_rate(self):
-        return self.m_rate
-    def set_rate(self,rt):
-        with self.lock:
-            self.m_rate = rt
-
-    def set_pid_p(self,P):
-        with self.lock:
-            self.pid_P = P
-            
-    def set_pid_i(self,I):
-        with self.lock:
-            self.pid_I = I
-
-    def set_pid_d(self,D):
-        with self.lock:
-            self.pid_D = D
-
-class ER_plot_component(HasTraits):
-    # Create the index
-    numpoints = 100
-    low = -5
-    high = 15.0
-    x = arange(low, high, (high-low)/numpoints)
-    plotdata = ArrayPlotData(x=x, y1=x, y2=x**2)
-
-    rate_plot = Plot(plotdata)
-    rate_plot.y_axis.title = "rate [nm/s]"
-    renderer = rate_plot.plot(("x", "y1"), type="line", color="blue", width=2.0)[0]
-
-    thickness_plot = Plot(plotdata)
-    thickness_plot.index_range = rate_plot.index_range
-    thickness_plot.y_axis.title = "thickness [nm]"
-    thickness_plot.plot(("x", "y2"),type="line", color="blue")
-    thickness_plot.x_axis.title = "time"
- 
-    current_plot = Plot(plotdata)
-    current_plot.index_range = rate_plot.index_range
-    current_plot.y_axis.title = "Current [A]"
-    current_plot.plot(("x", "y2"),type="line", color="blue")
+#if __name__ == '__main__':
+if True:
+    # this should always work ;-)
+    from lib.er_data import DATA    
+    #global data 
+    data = DATA()
     
-    container = VPlotContainer(stack_order="top_to_bottom",background="lightgray")
-    container.spacing = 0
-    current_plot.padding_top = rate_plot.padding_bottom
-    
-    container.add(rate_plot)
-    container.add(current_plot)
-    container.add(thickness_plot)
-    view = View( Group(Item(name='container',editor=ComponentEditor(),show_label=False)))
+    import lib.er_pidcontrol as er_pid
+    data.pid = er_pid.pidcontrol()
+    # gui
 
-#===============================================================================
+    from lib.er_gui_controls import ER_State
+    ER_State.data = data
+    from lib.er_gui_plots import ER_plot_component
+    ER_plot_component.data = data
+    from lib.er_gui import EvapoRate
+    EvapoRate.data = data
+    print "main",dir(data)
 
-
-
-    
-class ER_State(HasTraits):
-    SetRate = Float(0.1)
-    SetThickness = Float(0.1)
-    Regulate = Button()
-    Acquire = Button()
-    P = Float(0.00,desc="set P parameter",auto_set=False, enter_set=True)
-    I = Float(0.00,desc="set I parameter",auto_set=False, enter_set=True)
-    D = Float(0.00,desc="set D parameter",auto_set=False, enter_set=True)
-    inout_thread = ""
-    acquire = False
-    
-    view = View(
-        Group(
-            VGroup(
-                Item(name='Acquire'),
-                show_border=True,
-                ),
-
-            VGroup(
-                Item(name='SetRate'),
-                Item(name='SetThickness'),
-                show_border=True,
-                ),
-            VGroup(
-                Item(name='P'),
-                Item(name='I'),
-                Item(name='D'),
-                Item(name='Regulate'),show_border=True,
-                )
-            )
-        )
-    
-    # some important actions
-    def _Acquire_fired(self):
-        self.Continuous = True
-        self.acquire = True
-        aq = self.inout_data()
-        print "Aquire fired, thread started"
-        
-    def _Regulate_fired(self):
-        print "Regulate fired"
-        #self.Regulate = True
-        
-    def _Regulate_default(self):
-        pass
-    def _Tctrl_changed(self):
-        pass
-
-    def _P_changed(self):
-        data.set_pid_p(self.P)
-    def _I_changed(self):
-        data.set_pid_i(self.I)
-    def _D_changed(self):
-        data.set_pid_d(self.D)
-        
-    # check thread
-    def inout_data(self):
-        if self.inout_thread and self.inout_thread.isAlive():
-            self.inout_thread.Continuous = False
-            #self.ER.Continuous = False
-        else:
-            self.inout_thread = InOutThread()
-            self.inout_thread.Continuous = self.Continuous
-            self.inout_thread.ER = self
-            self.inout_thread.start()
-
-
-class InOutThread(Thread):
-    "Remote operations"
-    def run(self):
-        m_rate = 0
-        SetRate = self.ER.SetRate
-        #thickness = self.ER.m_thickness
-        # aquire
-        # m_rate    = self.ER.m_rate
-        #outputCurrent = self.ER.outputCurrent
-        #regulateRate  = self.ER.regulateRate
-        
-        while self.Continuous:
-            sleep(0.001)
-            if self.ER.acquire:
-                try:
-                    "get Rate from xal"
-                    m_rate = data.r_dev.getRate()
-                    data.set_rate(m_rate)
-                except:
-                    print "no rate taken"
-                # calculate new output value
-                o_new_val, error = data.pid.update_Heat(m_rate)
-            if self.ER.regulate:
-                #out_current = get_current_from_PID()
-                # output something
-                HT=o_dev.set_output(o_new_val)
-                # save the error
-                data.set_pidE(error)
-                
-            if outputCurrent:
-                try:
-                    pass
-                except:
-                    print "no current output"
-
-
-# main object -> called by __main__
-class EvapoRate(HasTraits):
-    " Bring up the whole mess"
-    figure = Instance(ER_plot_component)
-    er_state = Instance(ER_State)
-    traits_view = View(HSplit(
-            Item('figure', style='custom',show_label=False,width=0.7),
-            Item('er_state',style='custom',show_label=False,label='State'),
-        ),
-        resizable=True, 
-        height=0.7, width=0.6,
-        title='EvapoRate v2 HR@KIT2012'
-      )
-
-    def _figure_default(self):
-        return ER_plot_component()
-    
-    def _er_state_default(self):
-        return ER_State()
-
-
-if __name__ == '__main__':
-    data = data()
-    pid =  er_pid.pidcontrol(data)
-    data.pid = pid
-    
+    # try loading devices ...
     try:
+        # import modules
+        import lib.er_ratedev as r_dev        
         data.r_dev = r_dev.Rate_Dev()
+        
     except:
-        print "Rate dev not found"
+        print ("ER: Rate dev not found, loading dummy.")
         class Rate_Dev(object):
             def get_Rate(self): return 0
         data.r_dev = Rate_Dev()
+    
+    try:
+        import lib.er_pycomedi as comedi
+        data.DAQ_Dev = comedi.IO_Dev()
         
+        # for the default NI-DAQ device, this configures AO0 and AO1 output channels (0,1)
+        data.DAQ_Dev.configure_ao()
+        #data.DAQ_Dev.output(0,float(var))
+        #DAQ_Dev.output(1,0)
+        #DAQ_Dev.close()
+    except:
+        print ("ER: comedi DAQ device not loaded, loading dummy.")
+        class DAQ_Dev(object):
+                    def output(self,channel, value): return 0
+        data.DAQ_Dev = DAQ_Dev()
+        
+    try:
+        import lib.er_combivac as combivac
+        data.P_Dev = Pressure_Dev()
+        #print data.P_Dev.setHV(on=False)
+        #print data.P_Dev.getPM()        
+    except:
+        print ("ER: combivac device not loaded, loading dummy.")
+        from random import random
+        class P_Dev(object):
+            def getPM(self): return random()*1e-5
+        data.P_Dev = P_Dev()
+        #data.P_Dev.getPM()
     title="EvapoRate v2 HR@KIT/2012"
-    ER = EvapoRate()
+    ER = EvapoRate(data=data)
     ER.configure_traits()
-
-
-
-"""
-#update the values in the storage
-            
-            self.DATA.set_last_Res(R)
-            T = self.RBR.get_T_from_R(R)
-            self.DATA.set_Temp(T)
-            ## for do not heat to much the mixing chamber
-            if T > 0.7:
-                self.HTR.set_Heat(0)
-                self.DATA.set_ctrl_Temp(0)
-                
-            NHW,error = self.DATA.PID.update_Heat(T)
-            # set the new heating power from pid
-            HT=self.HTR.set_Heat(NHW)
-            self.DATA.set_Heat(HT)
-            if self.DATA.debug:
-                print "T=%.2fmK R=%.2fOhm Heat %0.4f(V) %.4f(uW)" % (T*1000,R,HT,HT**2/480*1e6)
-            self.DATA.set_pidE(error)
-"""            
